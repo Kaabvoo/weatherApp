968de50c534d9cc34092dd175996cc6aa5f5c217
Kaabvoo <jgcavazos96@hotmail.com>
2018-05-18 11:50:32 -0500

    Se agrego el constructor y edito el css

--- src/Container/container.js
+++ src/Container/container.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './container.css';
 
 export default class Container extends Component {
+  constructor(){
+    super()
+    weathers: {
+      city: '';
+      temp: '';
+      icon: '';
+    }
+  }
+
+
 
   render() {
     return (
       <div className="container">
-        HelloWorld!
+        
       </div>
     );
   }
